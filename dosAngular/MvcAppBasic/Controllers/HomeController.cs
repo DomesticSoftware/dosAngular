cb8cab0383d25cc1ed9a1c3ad2e79dacdb9c9b08
realize-intra\asaridakis <asaridkais@realize.gr>
2014-07-07 17:50:37 +0300

    !

---
+++ dosAngular/MvcAppBasic/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcAppBasic.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult CustomersIndex()
+        {
+            return View();
+        }
+
+        public ActionResult DashBoardIndex()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ dosAngular/MvcAppBasic/Scripts/App/angualrJSApp.js
@@ -0,0 +1,47 @@
+﻿debugger;
+var angularJSApp = angular.module('angularJSApp', ['ngRoute']);
+
+
+angularJSApp.config(function ($locationProvider, $routeProvider) {
+
+    //$locationProvider.decorator('$sniffer', function ($delegate) {
+    //    $delegate.history = false;
+    //    return $delegate;
+    //});
+
+    $locationProvider.html5Mode(false);
+    //$locationProvider.hashPrefix('!');
+    
+    $routeProvider
+       .when('/', {
+           templateUrl: '/Home/DashBoardIndex',
+           controller: 'DashBoardCtrl'
+        })
+      .when('/Πελάτες', {
+          templateUrl: '/Home/CustomersIndex',
+          controller: 'CustomersIndexCtrl'
+      })
+      //.when('/tags/:tagId', {
+      //    templateUrl: '/partials/template2.html',
+      //    controller: 'ctrl2'
+      //})
+      //.otherwise({ redirectTo: '/DashBoard' }); //MvcAppBasic
+
+});
+
+angularJSApp.controller('bodyCtrl', function ($scope) {
+    $scope.tmpBody = "hello there";    
+});
+
+
+angularJSApp.controller('homeCtrl', function ($scope) {
+    //debugger;
+});
+
+angularJSApp.controller('CustomersIndexCtrl', function ($scope) {
+    //debugger;
+});
+
+angularJSApp.controller('DashBoardCtrl', function ($scope) {
+    //debugger;
+});
